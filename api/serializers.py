b17ae4cb9ea712c02d3c93193428889fb467594f
Samir Allous <samirxallous@gmail.com>
2019-10-10 14:08:45 +0100

    Add api aggregation and group by

--- api/serializers.py
+++ api/serializers.py
@@ -3,7 +3,24 @@ from rest_framework import serializers
 from .models import Metric
 
 
-class MetricSerializer(serializers.ModelSerializer):
+class DynamicFieldsModelSerializer(serializers.ModelSerializer):
+    """
+    A ModelSerializer that uses `fields` query parameter from request
+    to controls which fields should be displayed.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        fields = self.context['request'].query_params.get('fields')
+        if fields:
+            # Drop any fields that are not specified in the `fields`.
+            allowed = set(fields.split(','))
+            existing = set(self.fields)
+            for field_name in existing - allowed:
+                self.fields.pop(field_name)
+
+
+class MetricSerializer(DynamicFieldsModelSerializer):
     cpi = serializers.FloatField()
 
     class Meta:
--- api/views.py
+++ api/views.py
@@ -1,3 +1,5 @@
+from django.db.models import FloatField, Sum
+from django.db.models.functions import Cast
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, mixins, viewsets
 
@@ -7,8 +9,48 @@ from .serializers import MetricSerializer
 
 
 class MetricViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
-    queryset = Metric.objects.all()
     serializer_class = MetricSerializer
     filter_backends = (DjangoFilterBackend, filters.OrderingFilter)
     filterset_class = MetricFilter
     ordering_fields = '__all__'
+    group_by_field = 'groupby'
+    aggregation_field = 'fields'
+    group_by_fields = ['date', 'channel', 'country', 'os']
+    aggregation_fields = ['impressions', 'clicks',
+                          'installs', 'spend', 'revenue', 'cpi']
+
+    def get_queryset(self):
+        queryset = Metric.objects.all()
+        groups = self.request.query_params.get(self.group_by_field)
+        if groups:
+            queryset = self.perform_groub_by(queryset, groups.split(','))
+            aggregations = self.request.query_params.get(
+                self.aggregation_field)
+            if aggregations:
+                queryset = self.perform_aggregations(
+                    queryset, aggregations.split(','))
+        return queryset
+
+    def perform_groub_by(self, queryset, group_by_fields):
+        if group_by_fields:
+            fields = []
+            for field in group_by_fields:
+                if field in self.group_by_fields:
+                    fields.append(field)
+            if fields:
+                queryset = queryset.values(*fields)
+        return queryset
+
+    def perform_aggregations(self, queryset, aggregation_fields):
+        if aggregation_fields:
+            aggregations = {}
+            for field in aggregation_fields:
+                if field in self.aggregation_fields:
+                    if field == 'cpi':
+                        aggregations[field] = Sum(
+                            'spend')/Cast(Sum('installs'), FloatField())
+                    else:
+                        aggregations[field] = Sum(field)
+            if aggregations:
+                queryset = queryset.annotate(**aggregations)
+        return queryset
