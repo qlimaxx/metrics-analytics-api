8088b46433a2ecab61158bf4a863ebd9b3e64a72
Samir Allous <samirxallous@gmail.com>
2019-10-10 10:52:27 +0100

    Add Metric model and its migration

--- api/models.py
+++ api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Metric(models.Model):
+    date = models.DateField()
+    channel = models.CharField(max_length=50)
+    country = models.CharField(max_length=10)
+    os = models.CharField(max_length=20)
+    impressions = models.IntegerField()
+    clicks = models.IntegerField()
+    installs = models.IntegerField()
+    spend = models.FloatField()
+    revenue = models.FloatField()
+
+    @property
+    def cpi(self):
+        try:
+            return self.spend / self.installs
+        except ZeroDivisionError:
+            return 0
